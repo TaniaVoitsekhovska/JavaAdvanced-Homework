42c4902939951892a81b372a314da99ad8c55acf
TaniaVoitsekhovska <taniavoitsekhovska@gmail.com>
2020-04-07 20:14:15 +0300

    renamed Main class into Application

--- homework15/src/main/java/ua/lviv/home/Application.java
+++ homework15/src/main/java/ua/lviv/home/Application.java
@@ -6,7 +6,7 @@ import org.hibernate.Transaction;
 import java.util.Arrays;
 import java.util.HashSet;
 
-public class Main
+public class Application
 {
     public static void main( String[] args ) {
 
