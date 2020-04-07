8812204220f9f63734f2b963159c2d98c665f500
TaniaVoitsekhovska <taniavoitsekhovska@gmail.com>
2020-04-07 20:41:05 +0300

    added method for reading class data to Application class

--- homework15/src/main/java/ua/lviv/home/Application.java
+++ homework15/src/main/java/ua/lviv/home/Application.java
@@ -15,13 +15,13 @@ public class Application
 
         Transaction transaction = session.beginTransaction();
 
-        Post first = new Post("events");
+        Post first = new Post("review");
 
-        Comment comment1 = new Comment("lunch");
+        Comment comment1 = new Comment("John");
         comment1.setPost(first);
-        Comment comment2 = new Comment("dinner");
+        Comment comment2 = new Comment("Mary");
         comment2.setPost(first);
-        Comment comment3 = new Comment("birthday party");
+        Comment comment3 = new Comment("Ivan");
         comment3.setPost(first);
 
         first.setComments(new HashSet<>(Arrays.asList(comment1, comment2, comment3)));
@@ -31,6 +31,19 @@ public class Application
         session.flush();
         transaction.commit();
 
+        System.out.println();
+        printTransactionData(first.getId(),session);
+
         session.close();
+    }
+    
+    private static void printTransactionData(int id,Session session){
+
+        session=HibernateUtil.getSession();
+        Transaction transaction=session.beginTransaction();
+        Post post=session.get(Post.class,id);
+        transaction.commit();
+
+        System.out.println(post + " contains " + post.getComments());
     }
 }
