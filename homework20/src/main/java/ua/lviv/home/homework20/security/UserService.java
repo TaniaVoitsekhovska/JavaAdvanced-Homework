49f07b7fd2edb105f245b1e61f0f24546689df16
TaniaVoitsekhovska <taniavoitsekhovska@gmail.com>
2020-04-24 16:21:05 +0300

    added homework Spring Security to homework20

---
+++ homework20/src/main/java/ua/lviv/home/homework20/security/UserService.java
@@ -0,0 +1,40 @@
+package ua.lviv.home.homework20.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ua.lviv.home.homework20.entities.User;
+import ua.lviv.home.homework20.entities.UserRegisterRequest;
+import ua.lviv.home.homework20.entities.UserRole;
+import ua.lviv.home.homework20.repository.UserRepository;
+
+import java.util.Collections;
+import java.util.Set;
+
+@Service
+public class UserService {
+    private static final Set<UserRole> DEFAULT_USER_ROLES = Collections.singleton(UserRole.ROLE_USER);
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void save(UserRegisterRequest userRegisterRequest) {
+        User user = new User();
+        user.setEmail(userRegisterRequest.getEmail());
+
+        // default
+        user.setRoles(DEFAULT_USER_ROLES);
+        user.setUserName(userRegisterRequest.getUsername());
+
+        String encodePassword = passwordEncoder.encode(userRegisterRequest.getPassword());
+        user.setPassword(encodePassword);
+
+        userRepository.save(user);
+    }
+}
